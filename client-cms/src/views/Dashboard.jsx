0b3e2aea866c01d3e4c8000168904a14f7fc2c38
alwanWicaksono <alwanwicaksono1@gmail.com>
2022-11-06 15:54:07 +0700

    charge

--- client-cms/src/views/Dashboard.jsx
+++ client-cms/src/views/Dashboard.jsx
@@ -14,7 +14,7 @@ function Dashboard() {
         <Row className="mt-5">
           <h1 className="d-flex justify-content-start">Dashboard</h1>
           <h5 className="d-flex justify-content-start">
-            Activities you need to monitor to maintain customer’s satisfaction
+            Activities you need to monitor to maintain customer's satisfaction
           </h5>
         </Row>
         <Row className="mt-3">
